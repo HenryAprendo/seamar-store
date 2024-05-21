77452376609ce408bb2593e52007267a7ffaef15
HenryAprendo <hjsmipuc@gmail.com>
2024-05-21 15:30:19 -0500

    fiet: Servicio de almacenamiento del token con localstorage

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,8 +1,10 @@
-import { Inject, Injectable, inject } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Token } from '../interface/token.model';
 import { Observable, tap } from 'rxjs';
 import { API_URL } from '../../constant/constant';
+import { BrowserStorageService } from '../../services/browser-storage.service';
+import { TOKEN } from '../../config/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +13,14 @@ export class AuthService {
 
   private http = inject(HttpClient);
 
+  private storage = inject(BrowserStorageService);
+
   private apiUrl = `${API_URL}/auth`;
 
   login(email:string, password:string): Observable<Token> {
     return this.http.post<Token>(`${this.apiUrl}/login`,{ email, password })
       .pipe(
-        tap(response => console.log(response.access_token))
+        tap(response => this.storage.set(TOKEN, response.access_token))
       )
   }
 
---
+++ src/app/services/browser-storage.service.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable } from '@angular/core';
+import { BROWSER_STORAGE } from '../config/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BrowserStorageService {
+
+  constructor(@Inject(BROWSER_STORAGE) public storage: Storage) { }
+
+
+  get(key:string): string | null {
+    return this.storage.getItem(key);
+  }
+
+  set(key:string, value:string) {
+    this.storage.setItem(key,value);
+  }
+
+
+}
